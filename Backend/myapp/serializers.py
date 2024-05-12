dfe82d10037635e2a9fb1f635a45e9378a2e09dd
KimTaewon <gimtaewonbeon@gmail.com>
2024-05-12 22:10:10 +0900

    fix problem about write_only_fields

--- Backend/myapp/serializers.py
+++ Backend/myapp/serializers.py
@@ -1,20 +1,14 @@
 from . import models
 from rest_framework import serializers
-from .stub import analyze_emotion
 
 class DiarySerializer(serializers.ModelSerializer):
     class Meta:
         model=models.Diary
         fields=("title","content","id")
         read_only_fields=("id",)
-        write_only_fields=("content",)
-    
-    #def create(self, validated_data):
-    #    validated_data['emotion']=analyze_emotion(
-    #        content=validated_data['content'],
-    #        title=validated_data['title']
-    #    )
-    #    return super().create(validated_data)
+        extra_kwargs = {
+                'content': {'write_only': True}
+        }
     
     def update(self, instance, validated_data):
         raise Exception("cannot update Diary")
@@ -29,4 +23,6 @@ class DiaryEmotionSerializer(serializers.ModelSerializer):
     class Meta:
         model=models.DiaryEmotion
         fields=['diary','anger','sadness','anxiety','hurt','panic','happyness']
-        write_only_fields=['diary']
+        extra_kwargs = {
+                'diary': {'write_only': True}
+        }
