b6e0acd794c7d453fe877f2b4d77bd38f9865530
siwooJang <joy8417@naver.com>
2024-06-07 23:58:21 +0900

    Chatbot function build

--- Frontend/Webpage/next.config.js
+++ Frontend/Webpage/next.config.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
